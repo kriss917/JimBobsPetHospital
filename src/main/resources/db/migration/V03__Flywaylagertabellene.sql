3b9dbd29b9339ee6fc8ed5fd73a8e4dbd5583bbb
joealjunious-reginold <joeal.1013@gmail.com>
2025-10-20 15:56:46 +0200

    nye sql for å fikse tabellene

---
+++ src/main/resources/db/migration/V03__Flywaylagertabellene.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS owner (
+                                     id SERIAL PRIMARY KEY,
+                                     first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    email VARCHAR(255)
+    );
