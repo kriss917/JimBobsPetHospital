1f3e99cba1dcd9a9517ec70a9ba566da7169910e
krist <kristoffer.b.fernholt@gmail.com>
2025-10-20 12:24:58 +0200

    create project

---
+++ src/main/java/com/example/jimbobspethospital/JimBobsPetHospitalApplication.java
@@ -0,0 +1,13 @@
+package com.example.jimbobspethospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JimBobsPetHospitalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JimBobsPetHospitalApplication.class, args);
+    }
+
+}
